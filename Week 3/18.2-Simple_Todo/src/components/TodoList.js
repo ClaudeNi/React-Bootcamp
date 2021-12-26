fad12d52bde792aa79bd9752b0d0d9f1e295d4fe
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2021-12-26 13:04:58 +0200

    Add files via upload

--- Week 3/18.2-Simple_Todo/src/components/TodoList.js
+++ Week 3/18.2-Simple_Todo/src/components/TodoList.js
@@ -9,7 +9,7 @@ const ToDoList = (props) => {
 
     return (
         <div className="todo-list">
-            {props.text}{" "}
+            <span className={isCompleted && "completed"}>{props.text}</span>{" "}
             <div role={"button"} onClick={handleClick} className="btn">
                 {isCompleted ? <span>&#10004;</span> : <span>&#10008;</span>}
             </div>
